eb729d58213664df341b76068650f6afddb12faa
kschoos <kschoos@student.tugraz.at>
2020-06-01 22:30:59 +0200

    Initialized node app and installed dependencies

--- app.js
+++ app.js
@@ -3,8 +3,6 @@ var app = express();
 
 app.use(express.static('public'));
 app.use(express.static('node_modules/jquery/dist'));
-app.use(express.static('node_modules/bootstrap/dist'));
-app.use(express.static('node_modules/popper.js/dist/umd'));
 
 app.get('/', function (req, res) {
     // res.send()
